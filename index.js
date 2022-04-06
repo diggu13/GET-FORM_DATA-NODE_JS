ed886cb6666dc2e90290259cc0415591156963c8
Virender singh (Digvijay) <94375210+diggu13@users.noreply.github.com>
2022-04-06 17:36:55 +0530

    added files

--- index.js
+++ index.js
@@ -1,30 +1,67 @@
-const fs = require('fs');
 const http = require('http');
-var querystring = require('querystring')
-const server = http.createServer((req,res)=>{
-    if(req.url === '/home'){
-        fs.readFile('form.html', (err,data)=>{
-            if(err){
-                res.writeHead(404);
-                res.write('Contents you are looking are Not Found');
-            }else{
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write(data);
-            }
-            res.end();
-        })
-    }else if(req.url === '/homeSubmit' && req.method === 'POST'){
-        var data = '';
-        req.on('data',function(chunk){
-            data += chunk;
-        });
-        req.on('end',function(chunk){
-            var q = querystring.parse(data);
-            console.log(q)
-        })
-        res.end();
-    }
-})
-server.listen(3000,'localhost',()=>{
-    console.log('listen 3000')
-})
+const MongoClient = require('mongodb').MongoClient
+var formidable = require('formidable');
+var url = "mongodb+srv://projecttwo:projecttwo@cluster0.qt7ts.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+const server = http.createServer(function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS,POST, GET');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Content-Type','multipart/form-data');
+    if(req.method === "POST"){
+        console.log("hi");
+        const form = formidable({ multiples: true });
+  
+        form.parse(req, (err, fields, files) => {  
+          console.log(fields);
+          console.log(files);
+        if (err) {
+          res.writeHead(err.httpCode || 400, { 'Content-Type': 'text/plain' });
+          res.end(String(err));
+          return;
+        }
+        MongoClient.connect(url,function(err,db){
+          if(err) throw err;
+          var database = db.db('sampledata');
+          var data = {
+            image : files,
+            field :fields
+          }
+          if(data.image){
+            return database.collection('data').insertOne(data,
+              function(err,result){
+                if(err) throw err;
+                res.writeHead(200, { 'Content-Type': 'application/json' });
+                console.log(result)
+              }
+              )
+          }
+        
+        res.end(JSON.stringify({ fields, files }, null, 2));
+        })      
+      });
+        }
+        else{
+          console.log('this is GET request');
+          MongoClient.connect(url,function(err,db){
+            if(err) throw err;
+            var dbo = db.db('sampledata');
+            if(dbo){
+              return dbo.collection('data').find({}).toArray(function(err,result){
+                if(err) throw err;   
+                res.writeHead(200,{'Content-Type':'application/json'})         
+                res.end(JSON.stringify(result))
+              }) 
+            }      
+          })    
+        }
+});
+
+server.listen(3000, (err) => {
+  if (err) {
+    console.log('bad things');
+    return;
+  }
+  console.log('listening on port 3000');
+});
